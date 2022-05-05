1798de7ba77d95123f78a5d0a442a14c8ec5d90f
Adelemphii <demilitioowner@gmail.com>
2022-05-05 13:19:12 -0500

    Initial commit

---
+++ src/main/java/tech/adeelmphii/zeus/Zeus.java
@@ -0,0 +1,15 @@
+package tech.adeelmphii.zeus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Zeus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
